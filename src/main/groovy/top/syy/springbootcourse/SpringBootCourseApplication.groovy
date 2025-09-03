e3222680863c8a55f85911da72d56a566a4af36f
CSakura123 <3068195512@qq.com>
2025-09-03 11:05:29 +0800

    init :初始化 spring-boot-course 项目

---
+++ src/main/groovy/top/syy/springbootcourse/SpringBootCourseApplication.groovy
@@ -0,0 +1,13 @@
+package top.syy.springbootcourse
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootCourseApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseApplication, args)
+    }
+
+}

57c0d8907b7691b9191b0babb4afebf06764edff
CSakura123 <3068195512@qq.com>
2025-09-03 14:29:32 +0800

    first

--- spring-boot-week1/src/main/java/top/syy/Application.java
+++ spring-boot-week1/src/main/java/top/syy/Application.java
@@ -3,7 +3,7 @@ package top.syy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;1
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Application {

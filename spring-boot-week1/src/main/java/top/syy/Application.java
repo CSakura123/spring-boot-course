18165f4d19c43bacf026ce9dab4186624d598bd2
CSakura123 <3068195512@qq.com>
2025-09-03 13:38:54 +0800

    init: 初始化 spring-boot-course 项目

--- spring-boot-week1/src/main/java/top/syy/Application.java
+++ spring-boot-week1/src/main/java/top/syy/Application.java
@@ -3,7 +3,7 @@ package top.syy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;1
 @RestController
 @SpringBootApplication
 public class Application {

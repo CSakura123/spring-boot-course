e0c894c5b2d7550ae301b06c182664b3324201c6
CSakura123 <3068195512@qq.com>
2025-09-17 09:03:53 +0800

    docs:修改了RAEDME文档

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spring Boot 课程的学习仓库
-## 
+##
+##spring-boot-up MyBatis Plus 模块
--- spring-boot-week1/src/main/java/top/ccc/boot/week1/service/SmsService.java
+++ spring-boot-week1/src/main/java/top/ccc/boot/week1/service/SmsService.java
@@ -1,5 +1,8 @@
 package top.ccc.boot.week1.service;
 
+/**
+ * @author 小c
+ */
 public interface SmsService {
    void sendSms(String phone);
 }
